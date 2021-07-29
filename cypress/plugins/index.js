68dd03bec288b8b568423d59e92616aac114e7ab
Juan David Vera Usman <juan.vera1@u.icesi.edu.co>
2021-07-29 11:30:12 -0500

    typescript configurated. Step 4 completed (#2)
    Co-authored-by: ZornStolz <juan.vera1@correo.icesi.edu.co>

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,6 +12,8 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const wp = require('@cypress/webpack-preprocessor')
+
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -19,4 +21,10 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  const options = {
+    webpackOptions: require('../../webpack.config'),
+  }
+
+  on('file:preprocessor', wp(options))
 }
